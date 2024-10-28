22b6894fddaec7cf5fe1796687f58bf683db50fb
Sand1818 <sandiso.jele@gmail.com>
2024-10-29 01:16:02 +0200

    Header and Calendar (incomplete)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-///import logo from './logo.svg';
-///import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import React from 'react';
 import Login from './components/LoginPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './components/Home'; //Ignore red for now- working with error
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        {
-        /*<Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        */}
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
