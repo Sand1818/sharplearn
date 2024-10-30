9a6f8e40f2acc21882af5fd950a4cd7908d05da4
Sand1818 <sandiso.jele@gmail.com>
2024-10-30 05:58:18 +0200

    Adding progress route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Login from './components/Login/LoginPage';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './components/Home'; //Ignore red for now- working with error
+import Progress from './components/Progress/Progress';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/progress" element={<Progress />} />
       </Routes>
     </BrowserRouter>
   );
